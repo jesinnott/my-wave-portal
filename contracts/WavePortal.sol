60c4986509a59dcfc9e6b645cef2945e5fa0216d
Juan Sinnott <juan.emilio.sinnott@gmail.com>
2021-11-21 12:47:41 -0300

    improve functions

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,19 +5,26 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
-  uint256 totalWaves;
+  mapping(address => uint256) public waveCount;
+  uint256 public totalWaves;
 
   constructor() {
     console.log("Greetings from the blockchain!");
   }
 
   function wave() public {
+    waveCount[msg.sender] += 1;
     totalWaves += 1;
-    console.log("%s has waved!", msg.sender);
+    console.log("%s has waved %d waves!", msg.sender, waveCount[msg.sender]);
   }
 
   function getTotalWaves() public view returns (uint256) {
     console.log("We have %d total waves!", totalWaves);
     return totalWaves;
+  }
+
+    function getSenderWaves() public view returns (uint256) {
+    console.log("%s gave %d waves!", msg.sender, waveCount[msg.sender]);
+    return waveCount[msg.sender];
   }
 }

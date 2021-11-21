860a425f8fb0d997584f7b14e4badce740c0258c
Juan Sinnott <juan.emilio.sinnott@gmail.com>
2021-11-21 19:03:05 -0300

    update the contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,26 +5,36 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
-  mapping(address => uint256) public waveCount;
   uint256 public totalWaves;
+
+  event NewWave(address indexed from, uint256 timestamp, string message);
+
+  struct Wave {
+    address waver;
+    string message;
+    uint256 timestamp;
+  }
+
+  Wave[] waves;
 
   constructor() {
     console.log("Greetings from the blockchain!");
   }
 
-  function wave() public {
-    waveCount[msg.sender] += 1;
+  function wave(string memory _message) public {
     totalWaves += 1;
-    console.log("%s has waved %d waves!", msg.sender, waveCount[msg.sender]);
+    console.log("%s waved w/ message %s", msg.sender, _message);
+
+    waves.push(Wave(msg.sender, _message, block.timestamp));
+    emit NewWave(msg.sender, block.timestamp, _message);
+  }
+
+  function getAllWaves() public view returns (Wave[] memory) {
+    return waves;
   }
 
   function getTotalWaves() public view returns (uint256) {
     console.log("We have %d total waves!", totalWaves);
     return totalWaves;
-  }
-
-    function getSenderWaves() public view returns (uint256) {
-    console.log("%s gave %d waves!", msg.sender, waveCount[msg.sender]);
-    return waveCount[msg.sender];
   }
 }
